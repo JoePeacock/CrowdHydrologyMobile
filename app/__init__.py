dd0291d22b3497c8c0ca8d916dec1359de7a1f0f
JoePeacock <jpeacockinc@gmail.com>
2014-11-13 13:53:00 -0500

    added uploads folder and sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from werkzeug.utils import secure_filename
+from flask.ext.sqlalchemy import SQLAlchemy
 
 import config as c
 
@@ -15,6 +16,21 @@ import config as c
 app = Flask(__name__)
 app.secret_key = c.SECRET_KEY
 app.config['UPLOAD_FOLDER'] = c.UPLOAD_FOLDER
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 def allowed_file(filename):
