4cb2639896d82dc7dc8a6876607d4daa04116853
Joseph A Peacock <japeacoc@crowdhydrology.geology.buffalo.edu>
2014-11-26 17:31:44 -0500

    updated brackets in config due to compile error

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 CSRF_ENABLED = True
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif']
 SECRET_KEY = 'F5W4sSeiSsbU4lB+SdXX1sBS4cgk6ZUs1gstMNiSgyMMzkvcF0+IGVZCOLAkJbzCYPZeItyGgfL0JPkpsxOY2+BysVWCGEKssqT4zu4gAXws1DsPH4v0F/LIie0kevAFuneFVbqM3CdFy/IRvffXUzD8nhvjEVKbwhLpUediUXE='
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
